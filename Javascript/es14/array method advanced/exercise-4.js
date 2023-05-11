399a9e6176eb0213c9a57b6fb6fe7d085c2e8f77
Simonello <[sstrati1995gmail.com]>
2023-05-11 15:40:51 +0200

    svolto es14

---
+++ Javascript/es14/array method advanced/exercise-4.js
@@ -0,0 +1,62 @@
+function uncompletedNotes(notes) {
+  let newArr= [];
+  notes.forEach((element) => {
+    newArr.push(element.todos.filter(el=> {
+      return el.done === false
+    })
+  )  });
+   
+  return newArr
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+console.log(uncompletedNotes(notes)); 
