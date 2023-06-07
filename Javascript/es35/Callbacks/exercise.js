eb9a1e3268cc7a9d80a3eadf119f1999485e5d43
Simonello <[sstrati1995gmail.com]>
2023-06-07 23:54:38 +0200

    correto es 35

--- Javascript/es35/Callbacks/exercise.js
+++ Javascript/es35/Callbacks/exercise.js
@@ -3,8 +3,10 @@ function repeatHello(callback) {
 }
 
 
-function callback() {
+const callback = () => {
     console.log("Ciao");
 }
+
+//l'arrow function è una variante di scrivere le funzioni, serve a scrivere meno codice e rendere più leggibile il codice
 
 repeatHello(callback)
